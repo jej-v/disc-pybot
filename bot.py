67660c14bcfc6e8ae52ba5596d9788f41aebba12
jej <jejkeklel@gmail.com>
2022-03-26 11:24:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from discord.commands import Option
 
 bot = discord.Bot()
 
-
 #Just Quotes from taglines.txt on textfiles.com
 with open('quotes.txt', 'r') as f:
     all_lines = f.readlines()
@@ -21,7 +20,7 @@ async def ping(ctx):
     await ctx.respond("Pong!")
 
 # quotes
-@bot.slash_command(guild_ids=[your_guild_ids_here]])
+@bot.slash_command(guild_ids=[your_guild_ids_here])
 async def q(ctx):
     """Just Quotes from taglines.txt on textfiles.com"""
     x = random.randint(0,len(all_lines)-1)
@@ -29,7 +28,7 @@ async def q(ctx):
 
 
 # info command w option
-@bot.slash_command(guild_ids=[your_guild_ids_here]])
+@bot.slash_command(guild_ids=[your_guild_ids_here])
 async def info(
     ctx: discord.ApplicationContext,
     member: Option(discord.Member, "member", required=False)):
@@ -56,7 +55,7 @@ async def info(
     await ctx.respond(embed=embed)
 
 # Help Command
-@bot.slash_command(guild_ids=[your_guild_ids_here]])
+@bot.slash_command(guild_ids=[your_guild_ids_here])
 async def help(ctx):
     """Shows the command list."""
     embed = discord.Embed(
